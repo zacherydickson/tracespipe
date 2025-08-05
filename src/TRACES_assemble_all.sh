e2b0e8d224c88639fab931efbfca7b67f4c0a32f
Zachery Dickson <zachery.dickson@helsinki.fi>
2025-08-05 13:08:01 +0300

    Implemented resuming attempts in TRACES_assemble_all - De-novo is now in a broken state

--- src/TRACES_assemble_all.sh
+++ src/TRACES_assemble_all.sh
@@ -7,8 +7,16 @@
 # o_fw_pr.fq o_rv_pr.fq o_fw_unpr.fq o_rv_unpr.fq
 #
 # ASSEMBLE
+
+if [ "$#" -lt 3 ]; then
+    >&2 echo -e "Usage: $(basename "$0") Organ nThread bResume\n" \
+                "\to_(fw|rv)_(un)?pr.fq must exists in the pwd\n";
+    exit 1;
+fi
+
 Organ=$1; shift
 nThread=$1; shift
+BResume=$1; shift
 fq1p="o_fw_pr.fq"
 fq2p="o_rv_pr.fq"
 fq1u="o_fw_unpr.fq"
@@ -16,5 +24,10 @@ fq2u="o_rv_unpr.fq"
 UnPairArg=""
 [ -s "$fq1u" ] && UnPairArg="-s $fq1u"
 [ -s "$fq2u" ] && UnPairArg="$UnPairArg -s $fq2u"
-#Note the lack of quatation marks on the UnPair Arg is required
-spades.py --meta --threads "$nThread" --only-assembler -o "../output_data/TRACES_denovo_$Organ" -1 "$fq1p" -2 "$fq2p" $UnPairArg 
+OutDir="../output_data/TRACES_denovo_$Organ"
+
+#If we are not trying to resume, or the attempt to resume fails, run the assembly from scratch
+if [ "$BResume" -eq 0 ] || ! spades.py --restart-from last -o "$OutDir"; then
+    #Note the lack of quotation marks on the UnPair Arg is required
+    spades.py --meta --threads "$nThread" --only-assembler -o "$OutDir" -1 "$fq1p" -2 "$fq2p" $UnPairArg 
+fi
