3d1e6aa85c5bc45bc7eecbba4f207e9ab13c2aa9
Zachery Dickson <zachery.dickson@helsinki.fi>
2025-08-05 13:02:46 +0300

    Added resuming capabilities to the unused TRACES_assemble_mito scripts

--- src/TRACES_assemble_mito.sh
+++ src/TRACES_assemble_mito.sh
@@ -4,5 +4,16 @@
 # MT-o_fw_pr.fq MT-o_rv_pr.fq MT-o_fw_unpr.fq MT-o_rv_unpr.fq
 #
 # ASSEMBLE
-spades.py -t 16 --careful -o out_spades_$1 -1 MT-o_fw_pr.fq -2 MT-o_rv_pr.fq -s MT-o_fw_unpr.fq -s MT-o_rv_unpr.fq
+
+Label=$1; shift
+NThread=$1; shift
+BResume=$1; shift
+
+workDir="out_spades_$Label"
+
+#If not attempting to resume, or the attempt to resume fails run the asembly from scratch
+if [ "$BResume" -eq 0 ] || ! spades.py --restart-from last -t "$NThread" -o "$workDir"; then 
+    spades.py -t "$NThread" --careful -o "$workDir" -1 MT-o_fw_pr.fq -2 MT-o_rv_pr.fq -s MT-o_fw_unpr.fq -s MT-o_rv_unpr.fq
+fi
+
 #
