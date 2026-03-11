1f726ef5ea149ec105e6ba48f3096d10470f2251
Zachery Dickson <zachery.dickson@helsinki.fi>
2026-03-11 16:25:58 +0200

    Added workaround for bug in sdust creating out-of-range intervals

--- src/TRACES_lcr_filter.sh
+++ src/TRACES_lcr_filter.sh
@@ -10,8 +10,15 @@ fi
 #the lcr regions. Note: falcon ignores non-canonical bases
 function main {
     local fastaIn=$1; shift;
-    sdust "$fastaIn" | bedtools maskfasta -fi "$fastaIn" -bed /dev/stdin \
+    #There is a bug in sdust which can cause it to report intervals outside of sequences
+    # if there are N's in the sequences
+    #As a workaround we will take the intersect with actual genomic intervals 
+    samtools faidx "$fastaIn"
+    sdust "$fastaIn" |
+        bedtools intersect -a /dev/stdin -b <(awk '{print $1"\t0\t"$2}' "$fastaIn.fai") |
+        bedtools maskfasta -fi "$fastaIn" -bed /dev/stdin \
         -fo /dev/stdout -fullHeader
+    rm -f samtools "$fastaIn.fai"
 }
 
 #Actually run the script
